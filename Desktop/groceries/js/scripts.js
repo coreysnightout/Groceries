2d4b076f0932302bda5cbdfac25cb600f732fea0
corey redding and tyler allen <corey@>
2017-05-01 17:19:07 -0700

    remove : from id=List10 and add .hide() for form selector

--- Desktop/groceries/js/scripts.js
+++ Desktop/groceries/js/scripts.js
@@ -4,6 +4,7 @@ $(function(){
 
     var arrayList = ["List1", "List2", "List3", "List4", "List5", "List6", "List7", "List8", "List9", "List10"];
 
+
     var newArray = arrayList.map(function(List){
        return $("#" + List).val();
     });
@@ -11,8 +12,9 @@ $(function(){
     var anotherArray = newArray.sort();
 
     anotherArray.forEach(function(caps) {
-      $("ul").append("<li>" + caps + "</li>");
+      $("ul").append("<li>" + caps.toUpperCase() + "</li>");
     });
+    $("form").hide();
     $(".output").show();
   });
     // var upperCasedArray = newArray.map(function(capital) {
